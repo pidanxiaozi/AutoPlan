57828d34417123b6f2c0022dc09daada5b5306ec
weijuhong <610806045@qq.com>
2025-04-09 21:50:24 +0800

    39章，# conftest.py配置浏览器窗口大小,配置录制视频的窗口大小,   pytest.init文件中 --video=on # #启用视频录制功能，测试运行时会录制视频

--- data_module/my_data.py
+++ data_module/my_data.py
@@ -1,1 +1,33 @@
+class MYData:
+    """
+    53章
+    登录的封装 - 你的测试数据应该放在哪?
+    """
+    def __init__(self, local=True, excel=None, yaml=None, feishu=None):
+        self.local = local
+        self.excel = excel
+        self.yaml = yaml
+        self.feishu = feishu
 
+    def userinfo(self):
+        user = ""
+        if self.excel:
+            pass
+            # todo 把excel转换成字典的方法
+        elif self.yaml:
+            pass
+            # todo 把yaml转换成字典的方法
+        elif self.feishu:
+            pass
+            # todo 把feishu转换成字典的方法
+        else:
+            user = {"playwright": {"测试员": {
+                        "username": "admin",
+                        "password": "123456",
+                        "email": "admin@163.com",
+                        "phone": "12345678901",
+                        "address": "北京",
+                    }
+                }
+            }
+        return user
